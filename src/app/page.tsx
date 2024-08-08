31dbda875ca025ad4e2cfdf20231283f7acfc341
hamzah-syed <hamzah.syed17@gmail.com>
2024-08-08 11:22:06 +0500

    updated

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+function Homepage() {
+    return (
+        <div>
+            <button className="text-white bg-blue-500 font-light rounded-md text-4xl px-5  py-5" >Login</button>
+        </div>
+    )
+}
+
+export default Homepage
